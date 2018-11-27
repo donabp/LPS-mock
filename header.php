502063909c45469f0834cc14cb886affdad5f15a
anthony-pena-ap2 <33699889+anthony-pena-ap2@users.noreply.github.com>
2018-11-27 16:10:30 -0500

    DSD-618

--- header.php
+++ header.php
@@ -5,7 +5,6 @@
 <meta name="viewport" content="width=device-width, initial-scale=1.0">
 <link rel="alternate" media="only screen and (max-width: 1024px)" href="<?php bloginfo('url');?>">
 <link href="https://fonts.googleapis.com/css?family=Oswald:400,700|Raleway:300,400,700" rel="stylesheet">
-<link href="//maxcdn.bootstrapcdn.com/font-awesome/4.2.0/css/font-awesome.min.css" rel="stylesheet">
 
 <?php wp_head(); ?>
 </head>
